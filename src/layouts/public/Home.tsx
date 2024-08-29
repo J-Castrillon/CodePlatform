f0c477e9e82fc262d8b72ff75e8d53e2efea5ad1
Anderson <andersonmestra05@gmail.com>
2024-08-28 21:33:36 -0500

    Login

--- src/layouts/public/Home.tsx
+++ src/layouts/public/Home.tsx
@@ -1,10 +1,24 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 
 export const Home = () => {
   return (
     <div>
-        <h1>Estamos en home</h1>
-        <Outlet/>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <h1 className="text-5xl">Login</h1>
+        <form action="" method="post" >
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <label htmlFor="txtUser">Usario</label>
+            <input type="text" name="txtUser" id="txtUser" />
+          </div>
+          <div className="flex flex-3 flex-col justify-center">
+            <label htmlFor="txtPassword">Contraseña</label>
+            <input type="password" name="txtPassword" id="txtPassword" />
+          </div>
+          <button type="submit">Ingresar</button>
+        </form>
+      </div>
+
+      <Outlet />
     </div>
-  )
-}
+  );
+};
